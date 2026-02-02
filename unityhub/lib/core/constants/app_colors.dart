cfe91bc4a0d591a7f1d790f059d05b1e118d1a0c
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2026-02-02 12:19:55 +0600

    color change of the landing page

--- unityhub/lib/core/constants/app_colors.dart
+++ unityhub/lib/core/constants/app_colors.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF1877F2); // facebook-ish blue
-  static const bg = Color(0xFFF6F7FB);
+  static const primary = Color.fromARGB(255, 185, 147, 214); // facebook-ish blue
+  static const bg = Color.fromARGB(255, 143, 172, 148);
   static const card = Colors.white;
   static const textDark = Color(0xFF101828);
   static const textMuted = Color(0xFF667085);
